d15ed0b1ac58de10c78a5fa42c2777e54b24511b
Mark Levison <mark@agilepainrelief.com>
2014-12-03 10:55:10 -0500

    Add in the effect that adding a buildserver slows the rate of damage down from 4 per round to 2 per round.

--- TeamsGameTestDriven/src/com/agilepainrelief/teamsgame/Team.java
+++ TeamsGameTestDriven/src/com/agilepainrelief/teamsgame/Team.java
@@ -3,6 +3,7 @@ package com.agilepainrelief.teamsgame;
 public class Team {
 	private int capacity = 10;
 	private int sprint = 0;
+	private BuildServer buildServer;
 
 	public int getCapacity() {
 		return capacity;
@@ -10,11 +11,19 @@ public class Team {
 
 	public void executeSprint() {
 		sprint++;
-		if ((sprint > 1) && (capacity > 0)) {
-			capacity -= 4;
+		if ((sprint >= 2) && (capacity > 0)) {
+			if (buildServer == null) {
+				capacity -= 4;
+			} else {
+				capacity -= 2;
+			}
 			if (capacity < 0) {
 				capacity = 0;
 			};
 		}
+	}
+
+	public void addAction(BuildServer inBuildServer) {
+		buildServer = inBuildServer;
 	}
 }
