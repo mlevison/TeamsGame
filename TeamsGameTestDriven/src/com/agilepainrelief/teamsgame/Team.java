98b44d2ae84c0a580fab94f56bba9df48bbaa632
Mark Levison <mark@agilepainrelief.com>
2014-12-03 11:48:03 -0500

    Added Limited TeamMember on same floor effect

--- TeamsGameTestDriven/src/com/agilepainrelief/teamsgame/Team.java
+++ TeamsGameTestDriven/src/com/agilepainrelief/teamsgame/Team.java
@@ -4,6 +4,7 @@ public class Team {
 	private int capacity = 10;
 	private int sprint = 0;
 	private BuildServer buildServer;
+	private TeamMembersOnSameFloor teamMembersOnSameFloor;
 
 	public int getCapacity() {
 		return capacity;
@@ -12,11 +13,18 @@ public class Team {
 	public void executeSprint() {
 		sprint++;
 		if ((sprint >= 2) && (capacity > 0)) {
+			int changeInCapacity = -4;
+
 			if ((buildServer != null) && (buildServer.isInEffect(sprint))) {
-				capacity -= 2;
-			} else {
-				capacity -= 4;
+				changeInCapacity += 2;
 			}
+
+			if ((teamMembersOnSameFloor != null)
+					&& (teamMembersOnSameFloor.isInEffect(sprint))) {
+				changeInCapacity += 2;
+			}
+
+			capacity += changeInCapacity;
 			if (capacity < 0) {
 				capacity = 0;
 			};
@@ -24,5 +32,9 @@ public class Team {
 	}
 	public void addAction(BuildServer inBuildServer) {
 		buildServer = inBuildServer;
+	}
+
+	public void addAction(TeamMembersOnSameFloor inTeamMembersOnSameFloor) {
+		teamMembersOnSameFloor = inTeamMembersOnSameFloor;
 	}
 }
